ba105be412e82e1bbe85ea219111d60e3edd531a
Wu Bingwen <jerryigeek@gmail.com>
2017-04-26 10:56:26 +0800

    创建实验室时候，同步存储相关信息到用户表中备用

--- Application/Home/Controller/LabController.class.php
+++ Application/Home/Controller/LabController.class.php
@@ -19,13 +19,25 @@ class LabController extends Controller {
     //创建实验室数据库操作
     public function lab_add_db(){
         $LabModel=D('Lab');
+        $UserModel=M('User');
         if($LabModel->create()){
+            $uniq_id=uniqid();
+            $LabModel->id=$uniq_id;
             $LabModel->holder_id=session('uid');
             $LabModel->holder=session('fullname');
+            //赋值用户模型类
+            $UserModel->lab_id=$uniq_id;
+            $UserModel->lab_name=$LabModel->name;
+            $Condition['id']=session('uid');
+            //SQL事务
+            $LabModel->startTrans();
             $Result=$LabModel->add();
-            if($Result){
+            $ResultUser=$UserModel->where($Condition)->save();
+            if($Result && $ResultUser){
+                $LabModel->commit();
                 $this->success('创建实验室成功',__ROOT__.'/index.php/Home/Lab/my_lab');
             }else{
+                $LabModel->rollback();
                 $this->error($LabModel->getError());
             }
         }else{
