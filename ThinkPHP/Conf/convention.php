eb5e2bc3965717b6125a293c725117295dd4824f
xcg-code <1450152983@qq.com>
2018-03-14 15:14:45 +0800

    构建项目

--- ThinkPHP/Conf/convention.php
+++ ThinkPHP/Conf/convention.php
@@ -57,10 +57,10 @@ return  array(
     'DB_HOST'               =>  '127.0.0.1', // 服务器地址
     'DB_NAME'               =>  'paper_manager',          // 数据库名
     'DB_USER'               =>  'root',      // 用户名
-    'DB_PWD'                =>  '',          // 密码
-    'DB_PORT'               =>  '3306',        // 端口
-    'DB_PREFIX'             =>  'think_',    // 数据库表前缀
-    'DB_PARAMS'          	=>  array(), // 数据库连接参数    
+    'DB_PWD'                =>  'root',          // 密码
+    'DB_PORT'               =>  '3308',        // 端口
+	'DB_PREFIX'             =>  'think_',    // 数据库表前缀
+	'DB_PARAMS'          	=>  array(), // 数据库连接参数
     'DB_DEBUG'  			=>  TRUE, // 数据库调试模式 开启后可以记录SQL日志
     'DB_FIELDS_CACHE'       =>  true,        // 启用字段缓存
     'DB_CHARSET'            =>  'utf8',      // 数据库编码默认采用utf8
